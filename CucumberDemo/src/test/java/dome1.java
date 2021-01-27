76ba8f110ae0624764f80779367aae5cf2ffeb35
snow <616193510@qq.com>
2021-01-27 15:30:08 +0800

    first commit

---
+++ CucumberDemo/src/test/java/dome1.java
@@ -0,0 +1,32 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.And;
+import cucumber.api.PendingException;
+public class dome1 {
+
+@Given("^创建一个数据集$")
+public void 创建一个数据集() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^清空数据集$")
+public void 清空数据集() throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^添加 (\\d+) 和 (\\d+)$")
+public void 添加_和(int arg1, int arg2) throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^检查结果 (\\d+)$")
+public void 检查结果(int arg1) throws Exception {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+}
